0b00def6f5aec36bc0bedb73d8395697260adf52
Andy Wong <andykswong@outlook.com>
2022-04-10 18:32:21 -0700

    v0.1.0

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+#[cfg(feature = "std")]
+extern crate std;
+
+mod matrix;
+mod matrix_special;
+mod matrix_ops;
+mod quat;
+
+pub mod float_eq;
+pub mod scalar;
+pub mod transform;
+
+pub use float_eq::FloatEq;
+pub use matrix::Matrix;
+pub use matrix_special::*;
+pub use quat::{quat, Quaternion};
